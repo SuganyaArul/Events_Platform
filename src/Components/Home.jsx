9c4da0cdc85df9e2c40a2a34919f3ca55cc527d3
Suganya <suganyaarul15@gmail.com>
2024-10-28 14:35:32 +0000

    UI correction

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,7 +2,7 @@ export default function Home(){
     return (
         <section>
           <div className="home_div"> Find your favourite events </div>
-           <img className="home" src='/pexels.jpg'/>
+           <img className="home" src='/pexels.jpg' alt="image of home page"/>
         </section>
     )
 }
