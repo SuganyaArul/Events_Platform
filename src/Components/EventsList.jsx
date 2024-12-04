dd7696a810e6647aa6a173fa765757be6e177f87
Suganya <suganyaarul15@gmail.com>
2024-12-04 10:45:06 +0000

    cosmetic changes

--- src/Components/EventsList.jsx
+++ src/Components/EventsList.jsx
@@ -1,8 +1,35 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Event from "./Event";
 import Pagination from "./Pagination";
-export default function EventsList({events}){
-   
+import { getEvents } from "../Utils/api";
+import {useSearchParams} from "react-router-dom"
+import { Hourglass } from "react-loader-spinner";
+export default function EventsList({events,setEvents}){
+    const [searchParams]=useSearchParams()
+    const [isLoading,setIsLoading] = useState()
+    // const [searchQuery,setSearchQuery]=useState([])
+    
+    const topics=searchParams.get('topics')
+    const location=searchParams.get('location')
+    const searchQuery={
+        topics:topics,
+        location:location
+    }
+    
+    useEffect(()=>{
+      
+          setIsLoading(true)
+        getEvents(searchQuery).then((body)=>{
+            setEvents(body)
+            
+            setIsLoading(false)
+            
+        }).catch((error)=>{
+            setEvents(null)
+            setIsLoading(false)
+            
+        })
+    },[topics,location])
     //pagination
     const postsPerPage = 3;
     const [currentPage, setCurrentPage]=useState(1)
@@ -16,6 +43,15 @@ export default function EventsList({events}){
     const handlePagination = (pageNumber) => {
         setCurrentPage (pageNumber);
     };
+    if(isLoading){return <div><Hourglass
+        visible={true}
+        height="80"
+        width="80"
+        ariaLabel="hourglass-loading"
+        wrapperClass=""
+        colors={["#575757", "#949494"]}
+      />
+      <p>Events Loading, please wait...</p></div>}
     return(
         <>
         <ol>
@@ -31,6 +67,17 @@ export default function EventsList({events}){
         postsPerPage={postsPerPage}
         handlePagination={handlePagination}
         />:null}
+        {isLoading?<div className="loading">
+      <Hourglass
+        visible={true}
+        height="80"
+        width="80"
+        ariaLabel="hourglass-loading"
+        wrapperClass=""
+        colors={["#575757", "#949494"]}
+      />
+      <p>Events Loading, please wait...</p>
+    </div>:null} 
         </>
     )
 
